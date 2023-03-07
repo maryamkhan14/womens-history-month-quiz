8aac590992b2ab2433451d58d0ff8c8fce602e6c
Maryam Khan <maryam.khan@pcc.edu>
2023-03-06 22:07:43 -0800

    Shortened descriptions of quiz categories

--- src/components/QuizCategorySelector.jsx
+++ src/components/QuizCategorySelector.jsx
@@ -18,22 +18,10 @@ const QuizCategorySelector = () => {
         id="category"
         onChange={(e) => handleSelectChange(e)}
       >
-        <option value="firsts">
-          Recognize the trailblazer. This category is about women who were the
-          first to do something important, such as going to space.
-        </option>
-        <option value="icons">
-          Identify the icon. This category involves women who shattered the
-          glass ceiling - successful businesswomen, philanthropists, scientists,
-          and more.
-        </option>
-        <option value="quotes">
-          Know who said it. This category contains quotes by powerful women such
-          as Maya Angelou and Oprah Winfrey.
-        </option>
-        <option value="misc">
-          Miscellaneous - get questions from all of the above categories!
-        </option>
+        <option value="firsts">Recognize the trailblazer.</option>
+        <option value="icons">Identify the icon.</option>
+        <option value="quotes">Know who said it.</option>
+        <option value="misc">Randomize!</option>
       </select>
     </div>
   );
